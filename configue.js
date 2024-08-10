171be931e56b20673995c45a0f31027f63a6e2c2
fahadalijs <fahad9827022@gmail.com>
2024-08-10 20:58:24 +0500

    fire store

--- configue.js
+++ configue.js
@@ -1,18 +1,20 @@
 
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-  
-  const firebaseConfig = {
-    apiKey: "AIzaSyChKrDmbEW66i0bYiV8u9KrY-mQv9qPDHw",
-    authDomain: "fir-authentication-168a0.firebaseapp.com",
-    projectId: "fir-authentication-168a0",
-    storageBucket: "fir-authentication-168a0.appspot.com",
-    messagingSenderId: "691955894055",
-    appId: "1:691955894055:web:d87f404f82bdc9a6058b02",
-    measurementId: "G-6SBV21F73F"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js"; 
 
-  // Initialize Firebase
-  export const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
+const firebaseConfig = {
+  apiKey: "AIzaSyChKrDmbEW66i0bYiV8u9KrY-mQv9qPDHw",
+  authDomain: "fir-authentication-168a0.firebaseapp.com",
+  projectId: "fir-authentication-168a0",
+  storageBucket: "fir-authentication-168a0.appspot.com",
+  messagingSenderId: "691955894055",
+  appId: "1:691955894055:web:d87f404f82bdc9a6058b02",
+  measurementId: "G-6SBV21F73F"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- home.js
+++ home.js
@@ -1,6 +1,15 @@
 import {onAuthStateChanged , signOut  } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { auth } from "./configue.js";
+import { auth , db,  } from "./configue.js";
+import { collection, addDoc , getDocs ,  Timestamp , query, orderBy } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js"; 
 
+
+const form = document.querySelector("#form");
+const tittle = document.querySelector("#tittle");
+const discription = document.querySelector("#discription");
+const card = document.querySelector("#card");
+
+
+// User login or logout function
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -11,6 +20,8 @@ onAuthStateChanged(auth, (user) => {
     }
   });
   
+// logout function 
+
 const logout = document.querySelector("#logout-btn");
 logout.addEventListener("click" , ()=>{
     signOut(auth).then(() => {
@@ -21,6 +32,100 @@ logout.addEventListener("click" , ()=>{
       });
       
 })
+
+// getData from Firestore
+const arr = []
+async function getDataFromFirestore (){
+  arr.length = 0;
+  const q = query(collection(db ,"posts"), orderBy('postsDate', 'desc')); 
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    arr.push({...doc.data(), docId:doc.id});
+    console.log(doc.id);
+    
+  });
+  console.log(arr);
+  arr.map((item)=>{
+    card.innerHTML += ` <div class="card mt-2">
+            <div class="card-body">
+                <p><span class="h4">Tittle:</span>${item.tittle}</p>
+                <p><span class="h4">Discription:</span>${item.discription}</p>
+                <button type="button" id = "delete" class="btn btn-danger">Danger</button>
+                <button type="button" id = "update" class="btn btn-info">Info</button>
+
+
+            </div>
+          </div>` 
+  })
+
+  const del = document.querySelectorAll("#delete");
+  const upd = document.querySelectorAll("#update");
+
+  del.forEach((btn , index)=>{
+   btn.addEventListener('click' , ()=>{
+    console.log("delete called" , arr[index]);
+    
+   })
+   })
+
+  upd.forEach((btn , index)=>{
+   btn.addEventListener('click' , ()=>{
+    console.log("update called" , arr[index]);
+    
+   })
+   })
+
+ 
+  
+}
+
+getDataFromFirestore()
+
+
+
+
+
+
+// post Data on Firestore
+
+form.addEventListener('submit' , async (event)=>{
+  event.preventDefault();
+  card.innerHTML = ' '
+  console.log(tittle.value);
+  console.log(discription.value);
+  try {
+    const docRef = await addDoc(collection(db, "posts"), {
+      tittle: tittle.value,
+      discription: discription.value,
+      uid: auth.currentUser.uid, 
+      postsDate: Timestamp.fromDate(new Date()),
+
+
+
+    });
+    console.log("Document written with ID: ", docRef.id);
+    getDataFromFirestore ()
+
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
